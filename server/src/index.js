ea819391774a5df7e847b64879a99f8605e55a73
hachiyuki8 <caladuil@outlook.com>
2023-04-05 13:25:47 -0400

    set up express backend and database

---
+++ server/src/index.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { router } from './routes.js';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+const companyID = "flurry";
+
+// connect to db
+let db_url = null
+if (app.get('env') === 'development') {
+  console.log(`⚡️connecting to development db`)
+  db_url = 'mongodb+srv://xinyao:20001028@cluster0.kwf9tsb.mongodb.net/flurry_devdb?retryWrites=true&w=majority'
+} else if (app.get('env') === 'test') {
+  console.log(`⚡️connecting to test db (local)`)
+  db_url = 'mongodb://localhost:27017/flurry_testdb'
+} else if (app.get('env') === 'deploy_test') {
+  console.log(`⚡️connecting to test db (deploy)`)
+  db_url = 'mongodb+srv://xinyao:20001028@cluster0.kwf9tsb.mongodb.net/flurry_testdb?retryWrites=true&w=majority'
+}
+mongoose
+  .connect(db_url)
+  .catch(e => {
+    console.error('Connection error', e.message)
+  });
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/', (req, res) => {
+  res.send('Flurry Backend Server (Express + JS)');
+});
+
+// Add a list of allowed origins.
+// If you have more origins you would like to add, you can add them to the array below.
+const allowedOrigins = ['http://localhost:8080']; // TODO: ADD FRONTEND ORIGIN
+
+const options = {
+  origin: allowedOrigins
+};
+
+// Then pass these options to cors:
+app.use(cors(options));
+app.use(express.json())
+app.use('/api', router)
+
+
+const server = app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
+
+export { app, server, companyID } // for testing
---
+++ server/src/routes.js
@@ -0,0 +1,28 @@
+import { Router } from 'express'
+
+const router = Router()
+
+// const Users = new UsersController()
+// const Products = new ProductsController()
+// const Orders = new OrdersController()
+// const Merchants = new MerchantsController()
+
+// router.get('/users', Users.getUsers)
+// router.post('/users', Users.createUser)
+// router.get('/users/:username', Users.getUserByUsername)
+// router.delete('/users/:username', Users.deleteUserByUsername)
+// router.patch('/users/:username', Users.updateUserByUsername)
+// router.post('/users/:username/checkout', Users.checkoutUserCart)
+
+// router.get('/users/:username/history', Users.getUserOrderHistory)
+// router.get('/orders', Orders.getOrders)
+// router.get('/orders/:id', Orders.getOrderById)
+// router.post('/orders/:id/drone', Orders.assignDroneToOrder)
+// router.post('/orders/totals', Orders.calculateTotalPrice)
+
+// router.get('/products', Products.getProducts)
+// router.get('/products/:title', Products.getProductByTitle)
+
+// router.get('/merchants/drones', Merchants.getDrones)
+
+export { router }

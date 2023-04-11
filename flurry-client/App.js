25dbbb5ab32d84423be004984dc3e8f529c7869c
Roshni <rsurpur@andrew.cmu.edu>
2023-04-11 01:31:40 -0400

    added formatting and inputs for form

--- flurry-client/App.js
+++ flurry-client/App.js
@@ -5,8 +5,8 @@ import SelfReportScreen from './screens/SelfReportScreen'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      {/* <StatusBar style="auto" /> */}
       <SelfReportScreen />
     </View>
   );
--- flurry-client/screens/SelfReportScreen.js
+++ flurry-client/screens/SelfReportScreen.js
@@ -1,62 +1,121 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, SectionList } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 import { slipIncidents } from '../data/dummyData';
 
 export default class SelfReportScreen extends React.Component {
-  handleDateRangeChange = (startDate, endDate) => {
-    // Do something with the selected start and end dates
-    //console.log('Selected date range:', startDate, endDate);
-  };
-  handleDriverSelect = (driverName) => {
-    setSelectedDriver(driverName);
-  };
-//   const [text, onChangeText] = React.useState('Useless Text');
-// const [text, setText] = React.useState("");
+    constructor(props) {
+        super(props);
+        this.state = {
+          timestamp: new Date(),
+          location: "date",
+          vehicleID: 1,
+          slipScore: 0
+        };
+      }
+    
+
+      onSlipScoreChange = (event, selectedScore) => {
+        const currentscore = selectedScore;
+        this.setState({
+            slipScore: currentscore,
+        });
+      };
+
+      onLocationChange = (event, selectedLocation) => {
+        const currentLocation = selectedLocation;
+        this.setState({
+            location: currentLocation,
+        });
+      };
+
+
+      onVehicleIDChange = (event, selectedVehicle) => {
+        const currentVehicle = selectedVehicle;
+        this.setState({
+            vehicleID: currentVehicle,
+        });
+      };
+
+      handleSubmit = () => {
+        // Do something with the form data, e.g. send it to a server
+        console.log(`Input 1 value: ${this.timestamp}`);
+        console.log(`Input 2 value: ${this.location}`);
+        console.log(`Input 3 value: ${this.vehicleID}`);
+        console.log(`Input 4 value: ${this.slipScore}`);
+      };
+    
+
+  
   render() {
     return (
-        <View style={styles.container}> 
-        <TextInput
-        label="Timestamp"
-        value={"text"}
-        />
-
-    <TextInput
-        label="location"
-        value={"text"}
-        />
-
-<TextInput
-        label="veichle id"
-        value={"text"}
-        />
-
-<TextInput
-        label="slip score"
-        value={"text"}
-        />
-
-
-</View>
-        
+        <View style={styles.container}>
+        <Text style={styles.heading}>Form Screen</Text>
+        <View style={styles.inputContainer}>
+        <Text>Timestamp: </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="timestamp"
+            value={this.state.timestamp}
+            // onChangeText={handleInput1Change}
+          />
+           <Text>Location: </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="location"
+            value={this.state.location}
+            onChangeText={this.onLocationChange}
+          />
+          <Text>Vehicle ID: </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="vehicleID"
+            value={this.state.vehicleID}
+            onChangeText={this.onVehicleIDChange}
+          />
+          <Text>Slip Score: </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="slipScore"
+            value={this.state.slipScore}
+            onChangeText={this.onSlipScoreChange}
+          />
+        </View>
+        <TouchableOpacity style={styles.submitButton} onPress={this.handleSubmit}>
+        <Text style={styles.submitButtonText}>Submit</Text>
+      </TouchableOpacity>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 10,
-    marginTop: 30,
-  },
-  item: {
-    marginVertical: 10,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginVertical: 10,
-    marginBottom: 10,
-  },
-});
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      paddingHorizontal: 20,
+      paddingVertical: 30,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    heading: {
+      fontSize: 32,
+      fontWeight: 'bold',
+      marginBottom: 40,
+      textAlign: 'center',
+      color: '#333',
+    },
+    inputContainer: {
+      marginTop: 20,
+      width: '90%', // updated width
+      paddingHorizontal: 20,
+    },
+    input: {
+      backgroundColor: '#eee',
+      borderRadius: 10,
+      paddingVertical: 12,
+      paddingHorizontal: 16,
+      marginBottom: 16,
+      fontSize: 18,
+    },
+  });
+  

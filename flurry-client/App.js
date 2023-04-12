293e80beddafdb108e725dc130feb9c9c3d531f6
emmaline01 <emmaline.mai@gmail.com>
2023-04-12 15:45:41 -0400

    ci fixes

--- flurry-client/App.js
+++ flurry-client/App.js
@@ -1,14 +1,13 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 // import { Text, View } from "react-native";
 
 // import AnalyticsScreen from "./screens/AnalyticsScreen";
 // import MapScreen from "./screens/MapScreen";
-import MainTabNavigator from "./navigation/MainTabNavigator"
-import { NavigationContainer } from '@react-navigation/native';
+import MainTabNavigator from "./navigation/MainTabNavigator";
 import styles from "./style";
 
 export default function App() {
-
   return (
     <NavigationContainer>
       <MainTabNavigator />
--- flurry-client/navigation/MainTabNavigator.js
+++ flurry-client/navigation/MainTabNavigator.js
@@ -1,40 +1,43 @@
-import React from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+
 // import { Platform, TouchableHighlight, Text, Alert, View } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-import MapScreen from '../screens/MapScreen';
-import AnalyticsScreen from '../screens/AnalyticsScreen';
+import AnalyticsScreen from "../screens/AnalyticsScreen";
+import MapScreen from "../screens/MapScreen";
 
 const Tab = createBottomTabNavigator();
 export default function MainTabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen 
-        name="Slip Incident Map" 
+      <Tab.Screen
+        name="Slip Incident Map"
         component={MapScreen}
         options={{
           tabBarIcon: ({ focused }) => (
             <Ionicons
               color={focused ? "blue" : "gray"}
-              name={'location-outline'}
+              name={"location-outline"}
               size={26}
               style={{ marginBottom: -3 }}
             />
-          )
-        }}/>
-      <Tab.Screen 
-        name="Analytics" 
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Analytics"
         component={AnalyticsScreen}
         options={{
           tabBarIcon: ({ focused }) => (
             <Ionicons
               color={focused ? "blue" : "gray"}
-              name={'analytics'}
+              name={"analytics"}
               size={26}
               style={{ marginBottom: -3 }}
             />
-          )
-        }}/> 
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
